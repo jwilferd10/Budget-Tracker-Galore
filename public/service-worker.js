bfc1ad0a4e033301ff528411aa5d6f95d507c84e
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-09 21:14:53 -0800

    Feat: Skeletal structure of service worker created

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const APP_PREFIX = 'NAME';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [];
+
+// self.addEventListener('activate', function(e) {})
+
+// self.addEventListener('fetch', function (e) {})
