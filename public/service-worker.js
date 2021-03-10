1a1f296b9ab3d3329e918bfe573a7fb3ac40c59d
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-09 22:54:12 -0800

    Feat: Fetch request fleshed out further, however further modifications likely needed.

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,4 +44,21 @@ self.addEventListener('activate', function(e) {
     );
 });
 
-// self.addEventListener('fetch', function (e) {})
+// Tell the browser to check the cache when there's no network connection.
+self.addEventListener('fetch', function (e) {
+    // listen for the fetch event, log the URL of the requested resource, and then begin to define how we will respond to the request.
+    console.log('fetch request: ' + e.request.url)
+    e.respondWith (
+        // use .match() to determine if the resource already exists in caches
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+            
+        })
+    )
+})
