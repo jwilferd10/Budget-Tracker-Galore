ca96bfd44ab98cc1295248ddc50c8776159d790e
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-17 20:37:55 -0700

    Feat: Forgot to commit this, but changes were made all up and down the service-worker.js, it should be working and I want to test this on the live deployment.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,5 @@
 const CACHE_NAME = "budget_tracker"
-
-// const APP_PREFIX = 'BudgetTracker';
-// const VERSION = 'version_01';
-// const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = 'budget-tracker-cache-v1'
 
 // Define which files to cache
 // Files to cache would be the homepage, index html, index js, idb js, and styles
@@ -18,7 +15,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function(event) {
     event.waitUntil (
         caches.open(CACHE_NAME).then(cache => {
-            console.log(CACHE_NAME + 'Files successfully pre-cached!');
+            console.log(CACHE_NAME + 'files successfully pre-cached!');
             return cache.addAll(FILES_TO_CACHE);
         })
     );
@@ -31,50 +28,43 @@ self.addEventListener('activate', function(event) {
         caches.keys().then(keyList => {
             return Promise.all (
                 keyList.map(key => {
-                    
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Deleting old' + CACHE_NAME + 'cache data', key);
+                        return caches.delete(key);
+                    }
                 })
-            )
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch requests
+self.addEventListener('fetch', function (event) {
+    if (event.request.url.includes('/api/')) {
+        console.log('fetch request : ' + event.request.url)
+        event.respondWith (
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch( err => {
+                    return cache.match(event.request);
+                });
+            })
+        );
+        return;
+    }
+
+    event.respondWith (
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
         })
     )
-})
-
-// Clear out any old data from the cache and tell the service worker how to manage caches.
-// self.addEventListener('activate', function(e) {
-//     e.waitUntil(
-//         // // .keys() returns an array of all cache names which we're calling keyList.
-//         caches.keys().then(function (keyList) {
-//             let cacheKeeplist = keyList.filter(function (key) {
-//                 return key.indexOf(APP_PREFIX); 
-//             });
-//             cacheKeeplist.push(CACHE_NAME);
-
-//             return Promise.all (
-//                 keyList.map(function(key, i) {
-//                     if (cacheKeeplist.indexOf(key) === -1) {
-//                         console.log('deleting cache : ' + keylist[i]);
-//                         return caches.delete(keyList[i]);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
-
-// Tell the browser to check the cache when there's no network connection.
-// self.addEventListener('fetch', function (e) {
-//     // listen for the fetch event, log the URL of the requested resource, and then begin to define how we will respond to the request.
-//     console.log('fetch request: ' + e.request.url)
-//     e.respondWith (
-//         // use .match() to determine if the resource already exists in caches
-//         caches.match(e.request).then(function (request) {
-//             if (request) {
-//                 console.log('responding with cache : ' + e.request.url)
-//                 return request
-//             } else {
-//                 console.log('file is not cached, fetching : ' + e.request.url)
-//                 return fetch(e.request)
-//             }
-            
-//         })
-//     )
-// })
+});
