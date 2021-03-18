fd773bcf3819e893defc798468141e44d077bec5
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-17 22:46:45 -0700

    Feat: I'm getting weird issues setting up the PWA's, I'm not sure what it is

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,9 +6,18 @@ const DATA_CACHE_NAME = 'budget-tracker-cache-v1'
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
+    '/manifest.json',
     "/js/index.js",
     "/js/idb.js",
-    "/css/styles.css"
+    "/css/styles.css",
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
 // Install Service-Workers
